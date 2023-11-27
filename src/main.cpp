bb27fcd1e1f8d21a6e8e2bf615d8607a5660f9f5
layle <luca@marcelli.ch>
2023-11-27 21:12:29 +0100

    lol

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,6 @@
  
 EFI_STATUS EFIAPI EfiMain(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE* SystemTable)
 {
-    SystemTable->ConOut->OutputString(SystemTable->ConOut, L"Hello Worrld\n");
+    SystemTable->ConOut->OutputString(SystemTable->ConOut, L"Hello World\n");
     return EFI_SUCCESS;
 }
