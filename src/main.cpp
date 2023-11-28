886661ce9bba8819afbc1c11b5bde09b34eaee77
Layle <luca@marcelli.ch>
2023-11-28 21:41:50 +0100

    ayy

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,8 @@
 #include <efi.h>
- 
+
 EFI_STATUS EFIAPI EfiMain(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE* SystemTable)
 {
     SystemTable->ConOut->OutputString(SystemTable->ConOut, L"Hello World\n");
+    while(true) {}
     return EFI_SUCCESS;
 }
